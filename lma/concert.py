e42ce8d08ac7c2b1ec1c93a880bb7b462c455153
Chris Waters <xtifr.w@gmail.com>
2012-05-24 02:38:19 -0700

    Fixup download_concerts, add clear_new_concerts

--- lma/concert.py
+++ lma/concert.py
@@ -8,15 +8,16 @@ from . import progress
 # Concert database access
 #
 
-def download_concerts(aid, bar = progress.NullProgressBar):
+def download_concerts(artist, progbar = progress.NullProgressBar):
     """Download concert records for given artist from LMA."""
+    artist = str(int(artist)) # make sure we have an integer
     db = database.Db()
 
     # get the last update date
     c = db.cursor()
     c.execute("SELECT a.lmaid, a.aname, l.browsedate FROM artist AS a"
               "  LEFT JOIN lastbrowse AS l ON a.aid = l.aid"
-              "  WHERE a.aid = ?", (aid,))
+              "  WHERE a.aid = ?", (artist,))
     lmaid, aname, lastdate = c.fetchone()
 
     # form the archive query (including lastdate)
@@ -29,15 +30,28 @@ def download_concerts(aid, bar = progress.NullProgressBar):
     # create the progress bar callback
     callback = progress.ProgressCallback("Live Music Archive Download",
                                          "Retrieve %s Concert List" % aname,
-                                         bar)
+                                         progbar)
 
     # push the records into our database, with callback
-    c.executemany("INSERT OR IGNORE INTO concert (ctitle, lmaid, cyear, cdate)"
-                  "  VALUES (:title, :identifier, :year, date(:date))",
+    c.executemany("INSERT OR IGNORE INTO concert"
+                  " (ctitle, lmaid, cyear, cdate, artistid) VALUES"
+                  "  (:title, :identifier, :year, date(:date), %s)" % artist,
                   query.ProgressIter(cquery, callback))
 
-    # now update the last-updated field
+    # now update the artist's last-updated field
     c.execute("INSERT OR REPLACE INTO lastbrowse (aid, browsedate)"
-              "  VALUES (?, date('now'))", (aid,))
+              "  VALUES (?, date('now'))", (artist,))
     db.commit()
     c.close()
+
+#
+# reset new concert list for given artist
+#
+
+def clear_new_concerts(artist):
+    """Clear an artist's new concert list."""
+    artist = str(int(artist))        # make sure
+    db = database.Db()
+    db.execute("DELETE FROM newconcert WHERE cid IN "
+               "  (SELECT cid FROM concert WHERE artistid = ?)", (artist,))
+    db.commit()
