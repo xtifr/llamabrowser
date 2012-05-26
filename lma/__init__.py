0f7bf02f8af439bef10d8bdc3d99d6bd6975e846
Chris Waters <xtifr.w@gmail.com>
2012-05-25 20:46:21 -0700

    add basic description for lma module.

--- lma/__init__.py
+++ lma/__init__.py
@@ -1,3 +1,25 @@
+"""Module for access to the Internet Archive's Live Music Archive.
+
+Data is read from the Live Music Archive and stored internally in a
+SQLite database which can then be browsed using various specialized
+classes and methods provided by this module.
+
+Reading data from the Live Music Access is done through the use of the
+Query class.  Instances of Query can generate iterators which will
+step through records from the Archive's data, live.
+
+The ProgressCallback class is a base class used to create a progress
+bar while downloading.  It can be specialized for different UIs.  The
+ProgressIter class combines a ProgressCallback with a Query to make an
+iterator that automatically updates a progress bar.
+
+The functions download_artists and download_concerts do what they say.
+They build appropriate Query objects internally, and take an optional
+ProgressCallback instance as a parameter.
+
+The ArtistList and ConcertList classes are wrappers for the records
+stored in the local database."""
+
 __version__ = '0.1'
 
 from lma.query import (Query, ProgressIter, IDENTIFIER, TITLE, COLLECTION,
